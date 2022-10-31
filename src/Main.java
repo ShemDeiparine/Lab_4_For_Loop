6772e58625f79fec0fc7d727fcbe40507691e878
Shem Deiparine <shemdeiparine@Shems-MacBook-Air.local>
2022-10-31 08:53:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//Shem Deiparine
+//sdeipar1@umbc.edu
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Shem Deiparine");
+        System.out.println("sdeipar1@umbc.edu");
+        System.out.println("---------------------------------");
+        System.out.println("Lab4: For Loop");
+        System.out.println("---------------------------------");
+
+
+        int count = 0;
+        for(int i = 9; i >= count; i--){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
